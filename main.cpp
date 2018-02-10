7f1b91d9357860fcefc9ddfacbfdb172f77d8b2f
Filip <Filip_Hesse@yahoo.de>
2018-02-10 21:05:08 +0100

    Irgendeine Änderung

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "lib/easylogging/easylogging++.h"
 #include <gtest/gtest.h>
+#include "Sensors.h"
 
 INITIALIZE_EASYLOGGINGPP
 
@@ -8,7 +9,9 @@ int main(int argc, char**argv)
 {
     testing::InitGoogleTest(&argc, argv);
     std::cout << "Hello world!" << std::endl;
-
+    LOG(INFO) << "My Log hello world";
+    Sensors sensors;
+    sensors.
 
     return RUN_ALL_TESTS();
 }
